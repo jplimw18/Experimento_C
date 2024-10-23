7590332a01ad6ab5b2db341dbae8683e42c30c84
João Lima <jplima1853@gmail.com>
2024-10-22 21:24:06 -0300

    .

--- Twice/main.c
+++ Twice/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
+
+float sigmoidf(float x)
+{
+    return 1.f / (1.f + expf(-x)); // exp = número de euler
+}
 
 float train[][3] = {
     {0, 0, 0},
@@ -16,8 +22,8 @@ float cost(float w1, float w2)
     for(size_t i = 0; i < train_count; i++) {
         float x1 = train[i][0];
         float x2 = train[i][1];
-        float y = x1*w1 + x2*w2;
-        float d = y-train[i][1];
+        float y = sigmoidf(x1*w1 + x2*w2);
+        float d = y-train[i][2];
         result += d*d;
     }
     result /= train_count;
@@ -32,9 +38,22 @@ float rand_float(void)
 
 int main(void)
 {
-    srand(69);
-    float w1 = rand_float();
-    float w2 = rand_float();
+    srand(time(0));
+    float w1 = rand_float()*10 - 5;
+    float w2 = rand_float()*10 - 5;
 
-    printf("w1 = %f, w2 = %f\n", w1, w2);
+    float eps = 1e-1;
+    float rate = 1e-1;
+
+    for(size_t i = 0; i < 1000*1000; ++i) {
+        float c = cost(w1, w2);
+
+        float dw1 = (cost(w1 + eps, w2) - c) / eps;
+        float dw2 = (cost(w1, w2 + eps) - c) / eps;
+        w1 -= rate*dw1;
+        w2 -= rate*dw2;
+    }
+    printf("w1 = %f, w2 = %f, c = %f\n", w1, w2, cost(w1, w2));
+
+    return 0;
 }
